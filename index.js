2c5e42c54b5c1ea828e3491a15acfcce38a2d9c9
THAtha@1399 <karnipremasatish258@gmail.com>
2023-07-13 18:35:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const buttons = document.getElementById("bambino-pasta-readmore");
+const buttons2 = document.getElementById("bambino-pasta-knowmore");
+const buttons3 = document.getElementById("pasta-image-1");
+const buttons4 = document.getElementById("pasta-image-3");
+const buttons5 = document.getElementById("story-button");
+const buttons6 = document.getElementById("subscribe");
+
+buttons.addEventListener("click", () => {
+  alert(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus error non excepturi corporis laudantiumid beatae delectus voluptatem aut mollitia nemo Nihil suscipit cumque nobis harum eius vero fugit non"
+  );
+});
+buttons2.addEventListener("click", () => {
+  alert(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus error non excepturi corporis laudantiumid beatae delectus voluptatem aut mollitia nemo Nihil suscipit cumque nobis harum eius vero fugit non"
+  );
+});
+buttons3.addEventListener("click", () => {
+  alert(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus error non excepturi corporis laudantiumid beatae delectus voluptatem aut mollitia nemo Nihil suscipit cumque nobis harum eius vero fugit non"
+  );
+});
+buttons4.addEventListener("click", () => {
+  alert(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus error non excepturi corporis laudantiumid beatae delectus voluptatem aut mollitia nemo Nihil suscipit cumque nobis harum eius vero fugit non"
+  );
+});
+buttons5.addEventListener("click", () => {
+  alert(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus error non excepturi corporis laudantiumid beatae delectus voluptatem aut mollitia nemo Nihil suscipit cumque nobis harum eius vero fugit non"
+  );
+});
+buttons6.addEventListener("click", () => {
+  alert("Subscribed");
+});
